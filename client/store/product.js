3973f0bf89a74a7575a929cd386c9c768dcde136
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-10-30 12:12:54 -0400

    reducers and store updates

---
+++ client/store/product.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+
+// ACTION TYPES
+const GET_PRODUCTS = 'GET_PRODUCTS'
+const GET_ONE_PRODUCT = 'GET_ONE_PRODUCT'
+
+
+// INITIAL STATE
+const productsState = {
+    products: null,
+    selectedProduct: {name: '', price: '', description: '', imageURL: ''}
+}
+
+// ACTION CREATORS
+const getProducts = (products) => ({
+    type: GET_PRODUCTS,
+    products
+})
+
+const getOneProduct = (product) => ({
+    type: GET_ONE_PRODUCT,
+    product
+})
+
+//THUNKAROOOS
+export const fetchProducts = () => {
+    return async (dispatch) => {
+        const {data} = await axios.get('./api/products')
+        dispatch(getProducts(data))
+    }
+}
+
+
+export const fetchAProduct = (category) => {
+    return async (dispatch) => {
+        const {data} = await axios.get(`./api/products/${category}`)
+        dispatch(getOneProduct(data))
+    }
+}
+
+// REDUCERS
+const productsReducer = (state = productsState, action) => {
+    switch (action.type){
+        case GET_PRODUCTS:
+          return {...state, products: action.products}
+
+        case GET_ONE_PRODUCT:
+          return{...state, selectedProduct: action.product}
+
+        default:
+          return state
+    }
+}
+
+export default productsReducer

a5ec9fec25446c84445fed1a19414c5c7172a4c9
Bethany Walker <reklaw.bethany@gmail.com>
2018-10-30 16:17:46 -0400

    Got data from store, now need to work on filters

--- client/components/products-list/products-list.js
+++ client/components/products-list/products-list.js
@@ -4,13 +4,6 @@ import FilterBar from './filter-bar'
 import { connect } from 'react-redux'
 import { fetchProducts } from '../../store/product'
 
-const theProducts = [
-  { id: 1, category: "charcuterie board", name: "large charcuterie board", description: "this board includes 10 cheeses and 10 meats, and will feed up to 20 people.", price: 150, imageURL: 'https://image.ibb.co/jyrCNf/no-image-avaliable.png', stock: 5340 },
-  { id: 2, category: "charcuterie board", name: "small charcuterie board", description: "this board includes 3 cheeses and 3 meats, and will feed up to 6 people.", price: 50, imageURL: 'https://image.ibb.co/cK0E2f/small-charcuterie.png', stock: 324 },
-  { id: 3, category: "cheese", name: "smoked gouda", description: "this is 1lb. of smoke gouda cheese", price: 100, imageURL: 'https://image.ibb.co/ecJv8L/smoked-gouda.png', stock: 0 },
-  { id: 4, category: "cheese", name: "gouda", description: "this is 1lb. of gouda cheese", price: 100, imageURL: 'https://image.ibb.co/mEmMhf/gouda.png', stock: 234 },
-  { id: 5, category: "meat", name: "pepperoni", description: "this is 1lb. of pepperoni", price: 100, imageURL: 'https://image.ibb.co/jyrCNf/no-image-avaliable.png', stock: 2543 }
-]
 
 class ProductsListComp extends Component {
   constructor() {
@@ -22,14 +15,11 @@ class ProductsListComp extends Component {
     this.handleChange = this.handleChange.bind(this)
   }
 
-  componentDidMount() {
-    // this.setState({ products: theProducts })
-
-    const products = this.props.fetchProducts()
+  async componentDidMount() {
+    await this.props.fetchProducts();
     this.setState({
       products: this.props.products
     })
-    console.log('this.props.products', this.props)
   }
 
   render() {
@@ -48,25 +38,28 @@ class ProductsListComp extends Component {
     )
   }
   handleChange(whatToFilter) {
-    this.setState({ products: filter(whatToFilter), filterTitle: filterTitle(whatToFilter) })
+    this.setState({ products: this.filterProducts(whatToFilter), filterTitle: filterTitle(whatToFilter) })
   }
+
+  filterProducts(whatToFilter) {
+    if (whatToFilter === 'all') return this.state.products;
+    else if (whatToFilter === 'charcuterie boards') return (
+      this.state.products.filter(product => product.category === 'charcuterie board')
+    )
+    else if (whatToFilter === 'cheeses') return (
+      this.state.products.filter(product => product.category === 'cheese')
+    )
+    else if (whatToFilter === 'meats') return (
+      this.state.products.filter(product => product.category === 'meat')
+    )
+    else return (
+      this.state.products.filter(product => product.category === 'extras')
+    )
+  }
+
 }
 
-function filter(whatToFilter) {
-  if (whatToFilter === 'all') return theProducts
-  else if (whatToFilter === 'charcuterie boards') return (
-    theProducts.filter(product => product.category === 'charcuterie board')
-  )
-  else if (whatToFilter === 'cheeses') return (
-    theProducts.filter(product => product.category === 'cheese')
-  )
-  else if (whatToFilter === 'meats') return (
-    theProducts.filter(product => product.category === 'meat')
-  )
-  else return (
-    theProducts.filter(product => product.category === 'extras')
-  )
-}
+
 
 function filterTitle(whatToFilter) {
   if (whatToFilter === 'all') return 'All Products'

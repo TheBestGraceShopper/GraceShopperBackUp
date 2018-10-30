d9ca8306df81bfba2e922b28dd847ffaaeb0604f
Bethany Walker <reklaw.bethany@gmail.com>
2018-10-30 18:33:58 -0400

    Fixed filter

--- client/components/products-list/products-list.js
+++ client/components/products-list/products-list.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Product from './product'
 import FilterBar from './filter-bar'
 import { connect } from 'react-redux'
-import { fetchProducts } from '../../store/product'
+import { fetchProducts, filterProducts } from '../../store/product'
 
 
 class ProductsListComp extends Component {
@@ -10,7 +10,7 @@ class ProductsListComp extends Component {
     super()
     this.state = {
       filterTitle: 'All Products',
-      products: []
+      products: [],
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -23,13 +23,20 @@ class ProductsListComp extends Component {
   }
 
   render() {
+      let filteredProducts = this.state.products;
+      if (this.state.filterTitle === 'All Products') filteredProducts = this.state.products
+      else if (this.state.filterTitle === 'All Charcuterie Boards') filteredProducts = this.state.products.filter(product => product.category === 'charcuterie board')
+      else if (this.state.filterTitle === 'All Cheeses') filteredProducts = this.state.products.filter(product => product.category === 'cheese')
+      else if (this.state.filterTitle === 'All Meats') filteredProducts = this.state.products.filter(product => product.category === 'meat')
+      else filteredProducts = this.state.products.filter(product => product.category === 'extras')
+
     return (
       <div>
         <FilterBar handleChange={this.handleChange} />
         <h1>{this.state.filterTitle}</h1>
         <div id="outer-products-div">
           <div className="products">
-            {this.state.products.map(product => {
+            {filteredProducts.map(product => {
               return <Product key={product.id} product={product} />
             })}
           </div>
@@ -37,28 +44,10 @@ class ProductsListComp extends Component {
       </div>
     )
   }
-  handleChange(whatToFilter) {
-    this.setState({ products: this.filterProducts(whatToFilter), filterTitle: filterTitle(whatToFilter) })
+  async handleChange(whatToFilter) {
+    this.setState({filterTitle: filterTitle(whatToFilter) })
   }
-
-  filterProducts(whatToFilter) {
-    if (whatToFilter === 'all') return this.state.products;
-    else if (whatToFilter === 'charcuterie boards') return (
-      this.state.products.filter(product => product.category === 'charcuterie board')
-    )
-    else if (whatToFilter === 'cheeses') return (
-      this.state.products.filter(product => product.category === 'cheese')
-    )
-    else if (whatToFilter === 'meats') return (
-      this.state.products.filter(product => product.category === 'meat')
-    )
-    else return (
-      this.state.products.filter(product => product.category === 'extras')
-    )
-  }
-
 }
-
 
 
 function filterTitle(whatToFilter) {
@@ -70,7 +59,6 @@ function filterTitle(whatToFilter) {
 }
 
 const mapStateToProps = (state) => {
-  console.log("STATE!!!!!:", state)
   return {products: state.productsReducer.products}
 }
 

200b1a6fbc5bf5ef6498a3adb59f6349ad9c87e7
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-10-31 08:04:59 -0400

    Cart store created and Component started

---
+++ client/components/cart.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+
+import {fetchOrder} from '../store/order'
+import axios from 'axios'
+
+class Cart extends Component {
+    componentDidMount(){
+        this.props.fetchOrder()
+    }
+}
+
+
+const mapStateToProps = state => ({
+    cart: state.cart
+})
+
+const mapDispatchToProps = dispatch => ({
+    fetchOrder: () => dispatch(fetchOrder())
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Cart))

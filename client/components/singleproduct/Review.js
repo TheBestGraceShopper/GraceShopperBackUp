1c0d5d8a73d028cdf56c235e7e669b421e2ec635
SummerDeehan <summerdeehan@hotmail.co.uk>
2018-11-02 16:09:55 -0400

    review working completely

--- client/components/singleproduct/Review.js
+++ client/components/singleproduct/Review.js
@@ -3,42 +3,19 @@ import {connect} from 'react-redux'
 import { fetchReviews, postReview} from '../../store'
 
 class Review extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-      rating: '',
-      reviews: [],
-      productId: '',
-      userId: '',
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  componentDidMount() {
-    this.props.getReviews(this.props.productId);
-    this.setState({userId: this.props.userId, productId: this.props.productId})
-  }
-  handleSubmit (e) {
-    e.preventDefault(e);
-    const review = {text: this.state.text, rating: Number(this.state.rating), userId: this.state.userId, productId: this.state.userId}
-    this.props.addReview(review);
-  }
-  handleChange (e) {
-    this.setState({[e.target.name] : e.target.value})
-  }
+
   render() {
-    console.log(this.props.reviews)
+
     return (
     <div>
       <h3> Reviews: </h3>
       <button> Add Review </button>
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.props.handleSubmit}>
 
       <label htmlFor='rating'> Rating:  </label>
-          <input type ='text' name='rating' onChange={this.handleChange} value={this.state.rating}/>
+          <input type ='text' name='rating' onChange={this.props.handleChange} value={this.props.state.rating}/>
           <label htmlFor='text'> Review:  </label>
-          <input type ='text' name='text' onChange={this.handleChange} value={this.state.text}/>
+          <input type ='text' name='text' onChange={this.props.handleChange} value={this.props.state.text}/>
 
           <button type="submit"> Submit Review</button>
       </form>
@@ -53,17 +30,5 @@ class Review extends React.Component {
   }
 }
 
-const mapState = state => {
-  return ({
-    reviews: state.review.reviews,
-    userId: state.user.id
-  })
-}
-const mapDispatch = dispatch => {
-  return ({
-    getReviews: (productId) => dispatch(fetchReviews(productId)),
-    addReview: (review) => dispatch(postReview(review)),
-  })
-}
 
-export default connect(mapState, mapDispatch)(Review);
+export default Review;
--- client/components/singleproduct/SingleProduct.js
+++ client/components/singleproduct/SingleProduct.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchAProduct} from '../../store'
-
+import { fetchAProduct, fetchReviews, postReview} from '../../store'
 import Review from './Review'
 import AddToCart from '../cart/AddToCart'
 
 
 class SingleProduct extends React.Component {
-    componentDidMount() {
-      this.props.getAProduct(this.props.match.params.productId)
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      rating: '',
+      productId: '',
+      userId: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+    async componentDidMount() {
+      const productId = this.props.match.params.productId
+      await this.props.getAProduct(productId)
+      await this.props.getReviews(productId);
+      this.setState({userId: this.props.userId, productId: productId});
+    }
+    handleSubmit (e) {
+      e.preventDefault(e);
+      const review = {text: this.state.text, rating: Number(this.state.rating), userId: this.state.userId, productId: this.state.productId}
+      console.log(review)
+      this.props.addReview(review);
+    }
+    handleChange (e) {
+      this.setState({[e.target.name] : e.target.value})
     }
 
     render() {
         const {selectedProduct} = this.props
-
         if (!selectedProduct.id) {
            return 'Loading the product...'
         }
@@ -29,7 +50,7 @@ class SingleProduct extends React.Component {
                 <AddToCart selectedProduct={selectedProduct} />
             </div>
             <div>
-              <Review productId={selectedProduct.id}/>
+              <Review state={this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} reviews={this.props.reviews} productId={selectedProduct.id}/>
             </div>
         </div>
         )
@@ -37,11 +58,15 @@ class SingleProduct extends React.Component {
 }
 
 const mapStateToProps = state => ({
-    selectedProduct: state.productsReducer.selectedProduct
+    selectedProduct: state.productsReducer.selectedProduct,
+    reviews: state.review.reviews,
+    userId: state.user.id
 })
 
 const mapDispatchToProps = dispatch => ({
-    getAProduct: (id) => dispatch(fetchAProduct(id))
+    getAProduct: (id) => dispatch(fetchAProduct(id)),
+    getReviews: (productId) => dispatch(fetchReviews(productId)),
+    addReview: (review) => dispatch(postReview(review))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)

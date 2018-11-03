e05cf42472fe3f97a0887ce0f1d22a3db791337b
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-11-03 16:07:06 -0400

    AddToCart fixed

--- client/components/cart/AddToCart.js
+++ client/components/cart/AddToCart.js
@@ -2,16 +2,10 @@ import React, {Component} from 'react'
 
 class AddToCart extends Component {
 
-    addCart(product){
-        let cart = [this.props]
-        cart.push(product)
-        localStorage.setItem('cart', JSON.stringify(cart))
-        this.setState(cart)
-    }
 
     render(){
         return (
-            <button type='button' onClick={() => this.addCart(this.props.products)}>
+            <button type='button' onClick={() => this.props.addToCart(this.props.selectedProduct)}>
             Add To Cart
             </button>
         )

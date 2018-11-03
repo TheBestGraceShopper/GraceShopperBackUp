d87141d03327385f10b3fd248f1fbc755aad9fb6
summerdeehan <42135521+summerdeehan@users.noreply.github.com>
2018-11-03 16:24:48 -0400

    Delete secret.js

--- secret.js
+++
@@ -1,3 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '549434176802-99iqisbmteftfb99cbnu9btc5ob206r2.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'Nc4L_Y-WJ87uRARIsODUiQvT'
-process.env.GOOGLE_CALLBACK = '/auth/google/callback'

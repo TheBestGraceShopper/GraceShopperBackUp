a7dafa7921b3389d0c3152d9734624a8cdce332e
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-03 21:37:51 -0400

    Change it so repeat items are not repeated in cart, and show amount

--- client/components/cart/CartPage.js
+++ client/components/cart/CartPage.js
@@ -5,21 +5,26 @@ import CheckoutPage from './CheckoutPage'
 
 
 const CartPage = () => {
-  const cartItems = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : []
-  
+  let cartItems = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : [];
+  cartItems = itemWithAmount(cartItems);
+  let cartItemNames = Object.keys(cartItems);
+  console.log('cartItems======', cartItems)
+
   return (
 
     <div>
       <h3>Your Shopping Cart</h3>
          <ol>
-            {cartItems.map((products) => (
-              <div key={products.id}>
+            {cartItemNames.map((productName) => (
+              <div key={cartItems[productName].id}>
                 <li >
-                  <Link to={`/products/${products.id}`}> {`${ products.name}`}</Link>
+                  <Link to={`/products/${cartItems[productName].id}`}> {`${ cartItems[productName].name}`}</Link>
                   <br />
-                  Price: {`${products.price}`}
+                  <p>Price: {`$${cartItems[productName].price * cartItems[productName].count}`}</p>
                   <br />
-                  <img src={products.imageURL} />
+                  <p>Amount: {cartItems[productName].count}</p>
+                  <br />
+                  <img src={cartItems[productName].imageURL} />
                 </li>
                 <br />
               </div>
@@ -37,3 +42,16 @@ const CartPage = () => {
   })
 
 export default connect(mapStateToProps)(CartPage);
+
+function itemWithAmount(items) {
+  const uniqueWtihCount = {}
+  items.forEach(item => {
+    if (!uniqueWtihCount[item.name]) {
+      uniqueWtihCount[item.name] = item;
+      uniqueWtihCount[item.name].count = 1
+    } else {
+      uniqueWtihCount[item.name].count++
+    }
+  })
+  return uniqueWtihCount;
+}

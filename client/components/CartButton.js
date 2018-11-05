b908cfc053a0ae9fef1d991ead9fa07d48715c97
Bethany Walker <GooMiHo@users.noreply.github.com>
2018-11-05 17:12:18 -0500

    Update CartButton.js

---
+++ client/components/CartButton.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';	
+import { Link } from 'react-router-dom';	
+import { connect } from 'react-redux';	
+ class CartButton extends Component {	
+  render() {	
+    const cartItemCount = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')).length : 0;	
+    return (	
+      <div id="shopping-cart" >	
+        <Link to="/cart">	
+          <img id="shopping-cart-image" src="https://image.ibb.co/e0KLJL/shopping-cart.png" alt="shopping-cart" />	
+          <p className="cart-number">{cartItemCount}</p>	
+        </Link> {/* currently directs to home, but will change to direct to cart*/}	
+      </div>	
+    )	
+  }	
+}	
+ const mapStateToProps = (state) => ({	
+  state	
+})	
+ export default connect(mapStateToProps)(CartButton);

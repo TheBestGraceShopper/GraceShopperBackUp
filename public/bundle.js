a6040e3d9b0b7e1605bcb10b132afebae68d16e3
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-11-05 11:47:00 -0500

    carouselFeature added

--- public/bundle.js
+++ public/bundle.js
@@ -642,6 +642,136 @@ exports.default = _default;
 
 /***/ }),
 
+/***/ "./client/components/ImageCarousel.js":
+/*!********************************************!*\
+  !*** ./client/components/ImageCarousel.js ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+var imgUrls = ["http://iamafoodblog.com/wp-content/uploads/2015/04/how-to-make-the-ultimate-cheese-plate-5w.jpg", "https://www.homebeautiful.com.au/media/13044/021117-cheese-platter-cath-muscat-2.jpg", "https://www.pompeian.com/sites/default/files/assets/images/recipes/brewing-happiness-floral-cheese-plate.jpg", "http://www.coastalvirginiamag.com/CoVa-Bay-Bites-Cheese-And-Charcuterie-board.jpeg", "https://ehealthforum.com/health/images/avatars/11699147425707699031013.jpeg"];
+
+var ImageCarousel =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(ImageCarousel, _Component);
+
+  function ImageCarousel(props) {
+    var _this;
+
+    _classCallCheck(this, ImageCarousel);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(ImageCarousel).call(this, props));
+    _this.state = {
+      currentImageIndex: 0
+    };
+    _this.nextSlide = _this.nextSlide.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    _this.previousSlide = _this.previousSlide.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    return _this;
+  }
+
+  _createClass(ImageCarousel, [{
+    key: "previousSlide",
+    value: function previousSlide() {
+      var lastIndex = imgUrls.length - 1;
+      var currentImageIndex = this.state.currentImageIndex;
+      var shouldResetIndex = currentImageIndex === 0;
+      var index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+      this.setState({
+        currentImageIndex: index
+      });
+    }
+  }, {
+    key: "nextSlide",
+    value: function nextSlide() {
+      var lastIndex = imgUrls.length - 1;
+      var currentImageIndex = this.state.currentImageIndex;
+      var shouldResetIndex = currentImageIndex === lastIndex;
+      var index = shouldResetIndex ? 0 : currentImageIndex + 1;
+      this.setState({
+        currentImageIndex: index
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("div", {
+        className: "carousel"
+      }, _react.default.createElement(Arrow, {
+        direction: "left",
+        clickFunction: this.previousSlide,
+        glyph: "\u25C0"
+      }), _react.default.createElement(ImageSlide, {
+        url: imgUrls[this.state.currentImageIndex]
+      }), _react.default.createElement(Arrow, {
+        direction: "right",
+        clickFunction: this.nextSlide,
+        glyph: "\u25B6"
+      }));
+    }
+  }]);
+
+  return ImageCarousel;
+}(_react.Component);
+
+var Arrow = function Arrow(_ref) {
+  var direction = _ref.direction,
+      clickFunction = _ref.clickFunction,
+      glyph = _ref.glyph;
+  return _react.default.createElement("div", {
+    className: "slide-arrow ".concat(direction),
+    onClick: clickFunction
+  }, glyph);
+};
+
+var ImageSlide = function ImageSlide(_ref2) {
+  var url = _ref2.url;
+  var styles = {
+    backgroundImage: "url(".concat(url, ")"),
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+  return _react.default.createElement("div", {
+    className: "image-slide",
+    style: styles
+  });
+};
+
+var _default = ImageCarousel;
+exports.default = _default;
+
+/***/ }),
+
 /***/ "./client/components/NavigationBar.js":
 /*!********************************************!*\
   !*** ./client/components/NavigationBar.js ***!
@@ -2578,6 +2708,8 @@ var _propTypes = _interopRequireDefault(__webpack_require__(/*! prop-types */ ".
 
 var _reactRedux = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 
+var _ImageCarousel = _interopRequireDefault(__webpack_require__(/*! ./ImageCarousel */ "./client/components/ImageCarousel.js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
@@ -2585,7 +2717,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 var UserHome = function UserHome(props) {
   var email = props.email;
-  return _react.default.createElement("div", null, _react.default.createElement("h3", null, "Welcome, ", email));
+  return _react.default.createElement("div", null, _react.default.createElement("h3", null, "Welcome, ", email), _react.default.createElement(_ImageCarousel.default, null));
 };
 /**
  * CONTAINER

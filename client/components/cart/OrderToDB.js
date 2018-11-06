ab34b8b8ad1b7e4d3ad190b115d379e1ea6fa2c6
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-05 19:43:57 -0500

    Backend routes for Order DB added

--- client/components/cart/OrderToDB.js
+++ client/components/cart/OrderToDB.js
@@ -1,62 +1,70 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux'
-import { findUser } from '../../store/user'
+import { me } from '../../store/user'
+import {mapCartItems} from '../../store/order'
 
-const fillOrder = () => {
+class OrderToDB extends React.Component {
+async componentDidMount () {
+await this.props.getUser();
+ //get orderId from db & add 1 for new orderId
+ // async function getNextOrderId() {
+ //   let id = await axios.get('/api/order/lastOrder');
+ //   return id + 1;
+ // }
 
-  //get orderId from db & add 1 for new orderId
-  // async function getNextOrderId() {
-  //   let id = await axios.get('/api/order/lastOrder');
-  //   return id + 1;
-  // }
+ //const nextOrderId = getNextOrderId();
 
-  //const nextOrderId = getNextOrderId();
+ //status is default
 
-  //status is default
+ //get userId from store
+//  const getUserId = async() => {
+//    const userId = await findUser();
+//    console.log('==========userId', userId);
+//  }
 
-  //get userId from store
-  // const getUserId = async() => {
-  //   const userId = await findUser();
-  //   console.log('==========userId', userId);
-  // }
+      let cartItems = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : [];
+      cartItems = itemWithAmount(cartItems);
+      let cartItemNames = Object.keys(cartItems);
 
-  //const userId = getUserId();
+        cartItemNames.map(async productName => {
 
- let user  = JSON.parse(localStorage.getItem('lsid'));
- console.log('==========userId', user);
+        const data = { totalPrice: cartItems[productName].price * cartItems[productName].count, userId: this.props.user.id }
+        console.log("data", data)
+        const order = await axios.post(`/api/order/add`, data);
 
-  //productsList comes from Local Storage & gets filtered to be productId & total
-  let cartItems = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : [];
-  cartItems = itemWithAmount(cartItems);
-  let cartItemNames = Object.keys(cartItems);
+        const productData = {productId: cartItems[productName].id, productQuantity: cartItems[productName].count, orderId: order.id};
 
-  cartItemNames.map(async productName => {
+        await axios.post(`/api/order/add_product_order`, productData)
 
-    const data = { totalPrice: cartItems[productName].price * cartItems[productName].count, userId, }
+      });
+      }
 
-    const order = await axios.post(`/api/order/add`, data);
+  render () {
+    return (<p> Thankyou for your purchase </p>)
+  }
 
-    const productData = {productId: cartItems[productName].id, productQuantity: cartItems[productName].count, orderId: order.id};
-
-    await axios.post(`/api/order/add_product_order`, productData)
-
-  });
 }
 
 function itemWithAmount(items) {
-  const uniqueWithCount = {}
-  items.forEach(item => {
-    if (!uniqueWithCount[item.name]) {
-      uniqueWithCount[item.name] = item;
-      uniqueWithCount[item.name].count = 1
-    } else {
-      uniqueWithCount[item.name].count++
-    }
-  })
-  return uniqueWithCount;
+ const uniqueWithCount = {}
+ items.forEach(item => {
+   if (!uniqueWithCount[item.name]) {
+     uniqueWithCount[item.name] = item;
+     uniqueWithCount[item.name].count = 1
+   } else {
+     uniqueWithCount[item.name].count++
+   }
+ })
+ return uniqueWithCount;
 }
 
-export default fillOrder;
 
-//export to cartPage?
+const mapState = state => ({
+  user: state.user
+})
+const mapDispatch = dispatch => ({
+  getUser: () => dispatch(me())
+})
+
+export default connect(mapState, mapDispatch)(OrderToDB)

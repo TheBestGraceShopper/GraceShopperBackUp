ac0c59de24eb46e2b93b2c781e5e54daf2130ac3
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-05 21:41:16 -0500

    New banner CSS added

--- client/components/CartButton.js
+++ client/components/CartButton.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';	
-import { Link } from 'react-router-dom';	
-import { connect } from 'react-redux';	
- class CartButton extends Component {	
-  render() {	
-    const cartItemCount = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')).length : 0;	
-    return (	
-      <div id="shopping-cart" >	
-        <Link to="/cart">	
-          <img id="shopping-cart-image" src="https://image.ibb.co/e0KLJL/shopping-cart.png" alt="shopping-cart" />	
-          <p className="cart-number">{cartItemCount}</p>	
-        </Link> {/* currently directs to home, but will change to direct to cart*/}	
-      </div>	
-    )	
-  }	
-}	
- const mapStateToProps = (state) => ({	
-  state	
-})	
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+ class CartButton extends Component {
+  render() {
+    const cartItemCount = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')).length : 0;
+    return (
+      <div id="shopping-cart" >
+        <Link to="/cart">
+          <img id="shopping-cart-image" src="https://image.ibb.co/i2CJTV/shopping-cart.png" alt="shopping-cart" />
+          <p className="cart-number">{cartItemCount}</p>
+        </Link> {/* currently directs to home, but will change to direct to cart*/}
+      </div>
+    )
+  }
+}
+ const mapStateToProps = (state) => ({
+  state
+})
  export default connect(mapStateToProps)(CartButton);
+

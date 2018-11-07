3aead7491c1db1667735a9d26a1782d061ea0691
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-06 21:51:23 -0500

    CSS in progress

--- client/components/OurStory.js
+++ client/components/OurStory.js
@@ -3,52 +3,49 @@ import React from 'react'
 const OurStory = () => {
   return (
 
-    <div className="about">
-      <div className="ourStory">
-      <h1>How The Board Came To Be</h1>
-      <p> Here is a little story about how we came to be!
-
-        Insert Team Photo
+    <div className="ourStory">
+      <div className="team">
+        <h1 className="how-title" >How The Board Came To Be</h1>
+        <p>
+          Here is a little story about how we came to be!
       </p>
-      </div>
-      <div className="Team">
-        <h1>Meat the Team</h1>
+        <h1 className="meet-the-title">Meat the Team</h1>
         <div className="teamContainer">
-        <figure>
-        <a href="https://www.linkedin.com/in/ayemachowdhury/">
-          <img src="https://media.licdn.com/dms/image/C4E03AQGm63zdAMZgzw/profile-displayphoto-shrink_200_200/0?e=1547078400&v=beta&t=KV60swP_LMJjoAWopH8R45rDXPAjVblAWPKoISPu3UM" />
-          </a>
-          <h5>Ayema Chowdhury</h5>
-          <span>Chief Pun Officer</span>
-          <p>Ayema spends most of her day thinking of cheese puns.
+          <figure>
+            <a href="https://www.linkedin.com/in/ayemachowdhury/">
+              <img src="https://media.licdn.com/dms/image/C4E03AQGm63zdAMZgzw/profile-displayphoto-shrink_200_200/0?e=1547078400&v=beta&t=KV60swP_LMJjoAWopH8R45rDXPAjVblAWPKoISPu3UM" />
+            </a>
+            <h5>Ayema Chowdhury</h5>
+            <span>Chief Pun Officer</span>
+            <p>Ayema spends most of her day thinking of cheese puns.
           </p>
-        </figure>
-        <figure>
-        <a href="https://www.linkedin.com/in/summerdeehan/">
-          <img src="https://media.licdn.com/dms/image/C4E03AQFmiPq7kPBeQQ/profile-displayphoto-shrink_800_800/0?e=1547078400&v=beta&t=IFxyhItzy_0bwd2B-OeNFMMekkncZHIvcqkc_i6fc1A" />
-       </a>
-          <h5>Summer Deehan</h5>
-          <span>Chief Expat Officer</span>
-          <p>Summer is British and hails from under Prince Charles.
+          </figure>
+          <figure>
+            <a href="https://www.linkedin.com/in/summerdeehan/">
+              <img src="https://media.licdn.com/dms/image/C4E03AQFmiPq7kPBeQQ/profile-displayphoto-shrink_800_800/0?e=1547078400&v=beta&t=IFxyhItzy_0bwd2B-OeNFMMekkncZHIvcqkc_i6fc1A" />
+            </a>
+            <h5>Summer Deehan</h5>
+            <span>Chief Expat Officer</span>
+            <p>Summer is British and hails from under Prince Charles.
           </p>
-        </figure>
-        <figure>
-        <a href="https://www.linkedin.com/in/conniejiayeelim/">
-          <img src="https://media.licdn.com/dms/image/C4E03AQE8Avr6dmZOPw/profile-displayphoto-shrink_800_800/0?e=1547078400&v=beta&t=SARPsH328KWi1G0DsQKvoyXr8MNgDXtjFOdKD8bH9mY" />
-        </a>
-          <h5>Connie Lim</h5>
-          <span>Chief Rind Officer</span>
-          <p>Connie spends painstaking hours to make sure the cheeses in our inventory are of the highest-quality. She also has the least knowledge of cheeses.
+          </figure>
+          <figure>
+            <a href="https://www.linkedin.com/in/conniejiayeelim/">
+              <img src="https://media.licdn.com/dms/image/C4E03AQE8Avr6dmZOPw/profile-displayphoto-shrink_800_800/0?e=1547078400&v=beta&t=SARPsH328KWi1G0DsQKvoyXr8MNgDXtjFOdKD8bH9mY" />
+            </a>
+            <h5>Connie Lim</h5>
+            <span>Chief Rind Officer</span>
+            <p>Connie spends painstaking hours to make sure the cheeses in our inventory are of the highest-quality. She also has the least knowledge of cheeses.
           </p>
-        </figure>
-        <figure>
-        <a href="https://www.linkedin.com/in/bethany-m-walker/">
-          <img src="https://image.ibb.co/hK7DCq/profile-pic.jpg" />
-          </a>
-         <h5>Bethany Walker</h5>
-          <span>Chief Food Officer</span>
-          <p>Bethany is prim and proper. She makes sure that your boards are perfect!</p>
-        </figure>
+          </figure>
+          <figure>
+            <a href="https://www.linkedin.com/in/bethany-m-walker/">
+              <img src="https://image.ibb.co/hK7DCq/profile-pic.jpg" />
+            </a>
+            <h5>Bethany Walker</h5>
+            <span>Chief Food Officer</span>
+            <p>Bethany is prim and proper. She makes sure that your boards are perfect!</p>
+          </figure>
         </div>
       </div>
     </div>

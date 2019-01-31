9543d2824e64c204064a1d70617555928af96bf9
Bethany Walker <reklaw.bethany@gmail.com>
2019-01-31 17:58:42 -0500

    Fixed Slideshow to work on mobil

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import SlideShow from 'react-image-show';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const { email } = props
 
   return (
     <div className="home-page">
       <h1 className="home-page-title">Welcome {email}</h1>
       {/* <img className="home-image" src="https://image.ibb.co/ftLjLA/Charcuterie-Board.png" /> */}
       <div id="slideshow">
-      <SlideShow
-      images = {[
-        'https://i.ibb.co/xzWVJBB/tray.jpg',
-        'https://i.ibb.co/D476fmV/tray2.jpg',
-        'https://i.ibb.co/TtK29Hz/tray3.jpg'
-      ]}
-      width = "920px"
-      imagesWidth = "100%"
-      imagesHeightMobile="56vw"
-      infinite indicators
-      />
-      {/* <SlideShow
-      images = {['https://static.vinepair.com/wp-content/uploads/2017/02/board-garlicbread.jpg','https://static.vinepair.com/wp-content/uploads/2017/02/board-eggplant.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-glutenfree.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-sepearte.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-bloodorange.jpg']}
-      width = "920px"
-      imagesWidth = "100%"
-      imagesHeightMobile="56vw"
-      infinite indicators
-    /> */}
-    </div>
+        <BrowserView>
+          <SlideShow
+            images={[
+              'https://i.ibb.co/xzWVJBB/tray.jpg',
+              'https://i.ibb.co/D476fmV/tray2.jpg',
+              'https://i.ibb.co/TtK29Hz/tray3.jpg'
+            ]}
+            width="920px"
+            imagesHeightMobile="56vw"
+            infinite indicators
+          />
+        </BrowserView>
+        <MobileView>
+          <SlideShow
+            images={['https://static.vinepair.com/wp-content/uploads/2017/02/board-garlicbread.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-eggplant.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-glutenfree.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-sepearte.jpg', 'https://static.vinepair.com/wp-content/uploads/2017/02/board-bloodorange.jpg']}
+            width="920px"
+            imagesWidth="100%"
+            imagesHeightMobile="56vw"
+            infinite indicators
+          />
+        </MobileView>
+      </div>
 
     </div>
   )
